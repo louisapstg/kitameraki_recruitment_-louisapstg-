8f8ef780a3c77bcd4dd60d15b81f655f38e6a149
Louis <louisapstg@gmail.com>
2023-11-25 11:53:42 +0700

    add get data method

---
+++ backend/src/data.js
@@ -0,0 +1,19 @@
+let data = [
+   {
+      title: 'Task 1',
+      description: "Lorem ipsum dolor sit amet, consectetur adip",
+      id: 1
+   },
+   {
+      title: 'Task 2',
+      description: "Lorem ipsum dolor sit amet, consectetur",
+      id: 2
+   },
+   {
+      title: 'Task 3',
+      description: "Lorem ipsum dolor sit amet, consect",
+      id: 3
+   }
+];
+
+module.exports = data
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,42 @@
 const http = require('http');
 const url = require('url');
-const port = 3000;
+const { successResponse, errorResponse } = require('./response');
+const data = require('./data');
 
-// In-memory data storage
-const taskList = [
-   {
-      id: 1,
-      title: 'Task 1',
-      description: 'Description for Task 1'
-   },
-   {
-      id: 2,
-      title: 'Task 2',
-      description: 'Description for Task 2'
-   },
-];
+const server = http.createServer((req, res) => {
+   const parsedUrl = url.parse(req.url);
+   const path = parsedUrl.pathname;
 
+   res.setHeader('Content-Type', 'application/json');
 
-server.listen(port, () => {
-   console.log(`Server is listening on port ${port}`);
+   if (req.method === 'GET') {
+      if (path === '/items') {
+         try {
+            successResponse(200, data, 'Items retrieved successfully', data.length, res);
+         } catch (e) {
+            errorResponse(404, e.message, res);
+         }
+      } else if (path.startsWith('/items/')) {
+         const itemId = parseInt(path.slice(7), 10);
+         const item = data.find((item) => item.id === itemId);
+
+         if (item) {
+            successResponse(200, item, 'Item retrieved successfully', data.length, res);
+         } else {
+            errorResponse(404, 'Item not found', res);
+         }
+      } else {
+         errorResponse(404, null, 'Not Found', null, res);
+      }
+   }
+
+   else {
+      errorResponse(405, 'Method not allowed', res);
+   }
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+   console.log(`Server running at http://localhost:${PORT}/`);
 });
