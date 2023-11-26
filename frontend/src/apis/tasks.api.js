f8108364b4cbb383efa742a31b368f399e0b3cb8
Louis <louisapstg@gmail.com>
2023-11-26 17:32:25 +0700

    add dynamic pagenumber to api

--- frontend/src/apis/tasks.api.js
+++ frontend/src/apis/tasks.api.js
@@ -1,51 +1,73 @@
 const TasksAPI = {
-   async getData() {
-      try {
-         const result = await fetch(
-            "http://localhost:3000/items?page=2&pageSize=14"
-         );
-         if (!result.ok) throw new Error("Network response failed to fetch items");
-         const response = await result.json();
-         return response
-      } catch (e) {
-         const { message } = e;
-         throw new Error(message);
-      }
-   },
+    async getData(pageNumber) {
+        try {
+            const result = await fetch(
+                `http://localhost:3000/items?page=${pageNumber}&pageSize=14`,
+            );
+            if (!result.ok)
+                throw new Error("Network response failed to fetch items");
+            const response = await result.json();
+            return response;
+        } catch (e) {
+            const { message } = e;
+            throw new Error(message);
+        }
+    },
 
-   async postData(data) {
-      try {
-         const result = await fetch("http://localhost:3000/items", {
-            method: "POST",
-            headers: {
-               "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data)
-         })
+    async postData(data) {
+        try {
+            const result = await fetch("http://localhost:3000/items", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
 
-         if (!result.ok) throw new Error("Network response failed to post data")
-         const response = await result.json()
-         return response
-      } catch (e) {
-         const { message } = e
-         throw new Error(message)
-      }
-   },
+            if (!result.ok)
+                throw new Error("Network response failed to post data");
+            const response = await result.json();
+            return response;
+        } catch (e) {
+            const { message } = e;
+            throw new Error(message);
+        }
+    },
 
-   async deleteData(id) {
-      try {
-         const result = await fetch(`http://localhost:3000/items/${id}`, {
-            method: "DELETE",
-         })
+    async editData(id, newdata) {
+        try {
+            const result = await fetch(`http://localhost:3000/items/${id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newdata)
+            })
 
-         if (!result.ok) throw new Error("Network response failed to delete data")
-         const response = await result.json()
-         return response
-      } catch (e) {
-         const { message } = e
-         throw new Error(message)
-      }
-   }
-}
+            if (!result.ok) throw new Error("Network response failed to edit data")
+            const response = await result.json()
+            return response
+        } catch (e) {
+            const { message } = e
+            throw new Error(message)
+        }
+    },
 
-export default TasksAPI
+    async deleteData(id) {
+        try {
+            const result = await fetch(`http://localhost:3000/items/${id}`, {
+                method: "DELETE",
+            });
+
+            if (!result.ok)
+                throw new Error("Network response failed to delete data");
+            const response = await result.json();
+            return response;
+        } catch (e) {
+            const { message } = e;
+            throw new Error(message);
+        }
+    },
+};
+
+export default TasksAPI;
