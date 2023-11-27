f802bccec8c3a8f5922fe81aa241256447951234
Louis <louisapstg@gmail.com>
2023-11-27 10:36:53 +0700

    add state for HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,18 @@
 import Form from "./../components/Form";
 import CardContainer from "./../components/CardContainer";
 import Navbar from "../components/Navbar";
+import { useState } from "react";
 const HomePage = () => {
+    const [pageNumber, setPageNumber] = useState(1);
     return (
         <div>
             <Navbar />
             <div className="flex p-5">
-                <CardContainer />
-                <Form />
+                <CardContainer
+                    pageNumber={pageNumber}
+                    setPageNumber={setPageNumber}
+                />
+                <Form pageNumber={pageNumber} />
             </div>
         </div>
     );
