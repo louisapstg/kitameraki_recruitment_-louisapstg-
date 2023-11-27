85fa58dfd9a830dbda21b50a48be01ab33ce4495
Louis <louisapstg@gmail.com>
2023-11-27 10:38:40 +0700

    add manual infinite scrooling when fetching data

--- frontend/src/components/CardContainer.jsx
+++ frontend/src/components/CardContainer.jsx
@@ -1,27 +1,94 @@
-import { useEffect } from "react";
+/* eslint-disable react/prop-types */
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useRef, useCallback } from "react";
 import Card from "./Card";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteTask, fetchTask } from "../stores/tasksSlice";
+import Swal from "sweetalert2";
 
-const CardContainer = () => {
+const CardContainer = ({ pageNumber, setPageNumber }) => {
     const dispatch = useDispatch();
+
     const datas = useSelector((state) => state.task.data);
-    //  const loading = useSelector((state) => state.task.loading);
+    const loading = useSelector((state) => state.task.loading);
+    const error = useSelector((state) => state.task.error);
+    const hasMore = useSelector((state) => state.task.hasMore);
+
+    const observer = useRef();
 
     useEffect(() => {
-        dispatch(fetchTask());
-    }, [dispatch]);
+        const fetchData = async () => {
+            try {
+                await dispatch(fetchTask(pageNumber));
+            } catch (err) {
+                console.error("Error fetching data:", err);
+            }
+        };
+        fetchData();
+    }, [dispatch, pageNumber]);
 
-    const handleDelete = async (id) => {
-        await dispatch(deleteTask(id));
-        dispatch(fetchTask());
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                try {
+                    await dispatch(deleteTask(id));
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your task has been deleted.",
+                        icon: "success",
+                        showConfirmButton: false,
+                    });
+                    setTimeout(() => {
+                        window.location.reload();
+                    }, 1500);
+                } catch (e) {
+                    console.error("Error deleting task", e);
+                }
+            }
+        });
     };
 
+    const lastTaskElementRef = useCallback(
+        (node) => {
+            if (observer.current) observer.current.disconnect();
+            observer.current = new IntersectionObserver((entries) => {
+                if (entries[0].isIntersecting && hasMore) {
+                    setPageNumber((prevPageNumber) => prevPageNumber + 1);
+                }
+            });
+            if (node) observer.current.observe(node);
+        },
+        [hasMore],
+    );
+
     return (
-        <div className="grid grid-cols-3 gap-4 p-5 w-2/3 max-h-[625px] overflow-y-auto">
-            {datas?.map((data, id) => {
-                return <Card key={id} data={data} onDelete={handleDelete} />;
+        <div className="grid grid-cols-3 gap-4 p-5 max-h-[550px] w-2/3 overflow-y-auto">
+            {datas?.map((data, index) => {
+                if (datas.length === index + 1) {
+                    return (
+                        <Card
+                            key={index}
+                            reff={lastTaskElementRef}
+                            data={data}
+                            onDelete={handleDelete}
+                        />
+                    );
+                } else {
+                    return (
+                        <Card key={index} data={data} onDelete={handleDelete} />
+                    );
+                }
             })}
+            {loading && <div>Loading...</div>}
+            {error && <div>{error}</div>}
         </div>
     );
 };
