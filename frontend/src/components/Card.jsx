d39879cee99c7c449587dfebada42b0fd527f3a8
Louis <louisapstg@gmail.com>
2023-11-27 10:37:11 +0700

    add edit modal in card

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -2,10 +2,12 @@
 import { DefaultButton } from "@fluentui/react";
 import { useState } from "react";
 import DetailModal from "./DetailModal";
+import EditModal from "./EditModal";
 
-const Card = ({ data, onDelete }) => {
+const Card = ({ data, onDelete, reff }) => {
     const { id, title, description } = data || {};
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [editModal, setEditModal] = useState(false);
 
     const handleDelete = async () => {
         try {
@@ -19,11 +21,15 @@ const Card = ({ data, onDelete }) => {
         setIsModalOpen(!isModalOpen);
     };
 
+    const handleEditModal = () => {
+        setEditModal(!editModal);
+    };
+
     return (
-        <div>
-            <div className="block max-w-xs max-h-40 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+        <div ref={reff}>
+            <div className="block max-w-xs max-h-40 p-6 bg-white border border-gray-200 rounded-sm shadow hover:bg-gray-100">
                 <div className="cursor-pointer" onClick={handleModal}>
-                    <div className="mb-2 text-lg font-bold tracking-tight text-gray-900">
+                    <div className="mb-2 text-lg font-bold tracking-tight text-gray-900 truncate">
                         {title}
                     </div>
                     <div className="mb-2 text-sm font-normal text-gray-700 truncate">
@@ -34,6 +40,7 @@ const Card = ({ data, onDelete }) => {
                     <DefaultButton
                         className="bg-warning hover:bg-yellow-500 text-primary"
                         text="Edit"
+                        onClick={handleEditModal}
                         allowDisabledFocus
                     />
                     <DefaultButton
@@ -49,6 +56,15 @@ const Card = ({ data, onDelete }) => {
                     data={data}
                     modal={isModalOpen}
                     handleModal={handleModal}
+                />
+            ) : (
+                ""
+            )}
+            {editModal ? (
+                <EditModal
+                    data={data}
+                    editModal={editModal}
+                    handleEditModal={handleEditModal}
                 />
             ) : (
                 ""
